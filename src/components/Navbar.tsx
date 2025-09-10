6e7c6a686bdc956ab34df100d06783aa9835a37a
KimJunSeo <rlawnstj901+2@gmail.com>
2025-09-10 19:02:59 +0900

    파비콘 경로 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Menu, X } from 'lucide-react';
 import { cn } from '../utils';
-import jkfavicon from '/favicon.png';
+import jkfavicon from '../assets/favicon.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
